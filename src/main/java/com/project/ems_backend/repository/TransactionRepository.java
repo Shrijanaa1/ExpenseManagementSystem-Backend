e33619f69865d0007c419d176fb66acbce2f4445
Shrijana Karki <srznakrki111@gmail.com>
2024-09-20 15:18:41 +0545

    Added findByDescriptionContaining method in TransactionRepository

--- src/main/java/com/project/ems_backend/repository/TransactionRepository.java
+++ src/main/java/com/project/ems_backend/repository/TransactionRepository.java
@@ -1,8 +1,13 @@
 package com.project.ems_backend.repository;
 
 import com.project.ems_backend.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
+    @Query("SELECT t FROM Transaction  t WHERE LOWER(t.description) LIKE LOWER(CONCAT('%', :description, '%'))")
+    Page<Transaction> findByDescriptionContaining(@Param("description") String description, Pageable pageable);
 }
