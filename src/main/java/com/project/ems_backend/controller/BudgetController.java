1297d17da36090380f9ccd382ad908ba1d85ac49
Shrijana Karki <srznakrki111@gmail.com>
2024-09-26 11:11:28 +0545

    Added REST APIs for getCategories in BudgetController

--- src/main/java/com/project/ems_backend/controller/BudgetController.java
+++ src/main/java/com/project/ems_backend/controller/BudgetController.java
@@ -1,9 +1,14 @@
 package com.project.ems_backend.controller;
 
 import com.project.ems_backend.model.Budget;
+import com.project.ems_backend.model.CategoryType;
 import com.project.ems_backend.service.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/budgets")
@@ -33,6 +38,15 @@ public class BudgetController {
     @PutMapping("/{id}")
     public Budget updateBudget(@PathVariable Long id, @RequestBody Budget budget){
         return budgetService.updateBudget(id, budget);
+    }
+
+    @GetMapping("/categories/{type}")
+    public ResponseEntity<List<String>> getCategories(@PathVariable("type") String type) {
+        List<String> categories = Arrays.stream(CategoryType.values())         // converts array into a stream for performing operations like filtering and mapping on it
+                .filter(cat -> cat.getType().name().equalsIgnoreCase(type))           //cat represents each CategoryType value (category) as it is being processed in the stream
+                .map(Enum::name)     //maps each element in the filtered stream to its name of the enum value
+                .toList();
+        return ResponseEntity.ok(categories);
     }
 
 }
