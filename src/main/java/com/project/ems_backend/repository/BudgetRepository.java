566df3cfe949e46dc49b152ba8c7be20d3279b34
Shrijana Karki <srznakrki111@gmail.com>
2024-09-26 11:06:54 +0545

    Added findByCategory method in BudgetRepository

--- src/main/java/com/project/ems_backend/repository/BudgetRepository.java
+++ src/main/java/com/project/ems_backend/repository/BudgetRepository.java
@@ -1,7 +1,13 @@
 package com.project.ems_backend.repository;
 
 import com.project.ems_backend.model.Budget;
+import com.project.ems_backend.model.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
+
+    //If no budget is found for the given category, the method returns an Optional.empty() instead of null. This makes code safer and helps avoid NullPointerExceptions
+    Optional<Budget> findByCategory(CategoryType category);
 }
