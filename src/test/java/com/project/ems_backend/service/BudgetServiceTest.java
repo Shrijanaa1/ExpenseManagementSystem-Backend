b47bef12a93130de4118e3bb1d3b2efbbf81d768
Shrijana Karki <srznakrki111@gmail.com>
2024-10-02 12:41:07 +0545

    Created unit test for getBudgetById method

--- src/test/java/com/project/ems_backend/service/BudgetServiceTest.java
+++ src/test/java/com/project/ems_backend/service/BudgetServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -29,12 +30,27 @@ class BudgetServiceTest {
 
     @Test
     void getAllBudgetsWithoutPagination() {
-        List<Budget> budgets = Arrays.asList(new Budget(), new Budget());
+        //Arrange(Setup necessary preconditions: creating objects, configuring mocks)
+        List<Budget> budgets = Arrays.asList(new Budget(), new Budget()); //Two objects of budget to test how method handles multiple budgets
         when(budgetRepository.findAll()).thenReturn(budgets);
 
+        //Act(Execute method being tested)
         List<Budget> result = budgetService.getAllBudgetsWithoutPagination();
 
+        //Assert(Verify expected outcome has occurred)
         assertEquals(2, result.size());
         verify(budgetRepository, times(1)).findAll();
     }
+
+    @Test
+    void getBudgetById() {
+        Budget budget = new Budget();
+        when(budgetRepository.findById(1L)).thenReturn(Optional.of(budget));
+
+        Budget result = budgetService.getBudgetById(1L);
+
+        assertNotNull(result);
+        verify(budgetRepository, times(1)).findById(1L);
+    }
+
 }
